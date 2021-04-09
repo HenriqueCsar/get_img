1bd9eeda056be1de136d119498902a0175e3ba5b
HenriqueCsar <60591111+HenriqueCsar@users.noreply.github.com>
2021-04-09 14:03:03 -0300

    Add files via upload

---
+++ img.js
@@ -0,0 +1,28 @@
+var request = require('request');
+
+///Pegando fotos
+
+function get_img(url, tipo) {
+
+    request(url, function resultados (error,response, body) {
+    if (response.statusCode == 200) {
+        const https = body
+
+        //src
+        const types = https.indexOf(tipo)
+
+
+        //htttps pegar de trás para frente o primeiro https
+        const httpsv = https.lastIndexOf('https:', types)
+        
+        //resultado url
+        var resultado = https.slice(httpsv,types+4);
+        console.log(resultado)
+        
+    }
+
+    });
+
+}
+
+exports.get_img = get_img
